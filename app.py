3b8da3e981af567cbb2f969041678a90ce82830e
samohiani <ohianisammy2005@gmail.com>
2025-04-11 23:07:15 +0100

    changes in port

--- app.py
+++ app.py
@@ -37,4 +37,5 @@ def index():
 import os
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
