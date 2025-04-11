1a1329c9ed43b8c5e6fcd5e1d98c580d36eef677
samohiani <ohianisammy2005@gmail.com>
2025-04-11 23:02:45 +0100

    address chsange

--- app.py
+++ app.py
@@ -34,5 +34,7 @@ def index():
         result = round(actual_size, 4)
     return render_template("index.html", result=result)
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
